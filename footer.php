b96275271fe8a90007a11711e29c17819a3575d8
T.Elouan <46402839+TusseauElouan@users.noreply.github.com>
2024-02-28 16:53:09 +0100

    update css

--- footer.php
+++ footer.php
@@ -24,5 +24,9 @@
             echo $message;
         }?></p>
     </form>
+    <div>
+        <a href="mention.php">Mention Légal</a>
+        <a href="condition.php">Condtion d'utilisation</a>
+    </div>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2708.181354992621!2d-2.2610377232932706!3d47.252156912385836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480568bfdf4e941b%3A0x36440760236165cc!2s1%20Bd%20de%20l&#39;Universit%C3%A9%2C%2044600%20Saint-Nazaire!5e0!3m2!1sfr!2sfr!4v1709133329146!5m2!1sfr!2sfr" width="600" height="450" style="border:1px solid black;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </footer>
